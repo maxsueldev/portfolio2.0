fe97a6a39c661d3f7bbd6fdc8373cb8432da1a5e
Maxsuel Fernando <maxsuel.dev@gmail.com>
2023-03-01 17:42:59 -0300

    Refatorando CSS para mobile first e flexbox

---
+++ enviar.php
@@ -0,0 +1,99 @@
+<?php 
+
+$email_envio = 'maxsuelf94@gmail.com'; //Email receptor
+$email_pass = ''; //Senha do email
+
+$site_name = 'Maxsuel Fernando - Desenvolvedor Web'; //Nome do site
+$site_url = ''; //Url do site
+
+$host_smtp = 'smtp.gmail.com'; //HOST SMTP Ex: smtp.domain.com.br
+$host_port = '587'; //Porta do host
+
+//Variáveis do formulário
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+
+//Conteúdo do formulário
+
+$body_content = "De: $nome \n Email: $email \n Mensagem: $mensagem";
+
+//Não mudar a partir daqui
+
+// if($_POST['leaveblank'] =! '' or $_POST['dontchange'] =! 'http://') {
+// 	print "<h2 style=\"
+// 	font-size: 1em;
+// 	margin-top: 20%;
+// 	text-align: center;
+// 	font-family: 'Helvetica', 'Arial', 'Sans-serif';
+// 	font-weight: normal;
+// 	color: #1b1b1b;
+// 	\"><center><span>Aconteceu algum erro!</span><p>Você pode tentar novamente ou enviar direto para " . $email_envio . "!</p></center></h2>";
+
+// 	print "<html style=\"background: #fff;\"></html>";
+// 	print "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $site_url . "'>";
+// }
+
+/* else */  
+
+if(isset($_POST['nome'])) { 
+	require('./mailerClass/PHPMailerAutoload.php');
+
+	$mail = new PHPMailer;
+	$mail->CharSet = 'UTF-8';
+
+	$mail->isSMTP();
+	$mail->Host = $host_smtp;
+	$mail->SMTPAuth = true;
+	$mail->Username = $email_envio;
+	$mail->Password = $email_pass;
+	$mail->Port = $host_port;
+
+	$mail->From = "maxsuelf94@gmail.com";
+
+	$mail->addAddress("maxsuelf94@gmail.com");
+
+	$mail->FromName = 'Formulário de Contato';
+	$mail->AddReplyTo($_POST['email'], $_POST['nome']);
+
+	$mail->WordWrap = 70;
+
+	$mail->Subject = 'Formulário - ' . $site_name . ' - ' . $_POST['nome'];
+
+	$mail->Body = $body_content;
+
+	if($mail->send()) {
+		header("location: https://maxsuelfernandob.000webhostapp.com");
+	} else {
+		header("location: https://maxsuelfernandob.000webhostapp.com");
+	}
+
+	// if(!$mail->send()) {
+	// 	print "<h2 style=\"
+	// 	font-size: 1.5em;
+	// 	margin-top: 20%;
+	// 	text-align: center;
+	// 	font-family: 'Inconsolata', 'Arial', 'Sans-serif';
+	// 	font-weight: normal;
+	// 	color: #fdc64b;
+	// 	\"><center><span>Aconteceu algum erro!</span><p>Você pode tentar novamente ou enviar direto para " . "contato@escolacreative.com" . "!</p></center></h2>";
+
+	// 	print "<html style=\"background: #fff;\"></html>";
+	// 	print "<meta HTTP-EQUIV='Refresh' CONTENT='10;URL=" . $site_url . "'>";
+	// } else {
+	// 	print "<h2 style=\"
+	// 	font-size: 1.5em;
+	// 	margin-top: 20%;
+	// 	text-align: center;
+	// 	font-family: 'Inconsolata', 'Arial', 'Sans-serif';
+	// 	font-weight: normal;
+	// 	color: #89bb50;
+	// 	\"><center><span>Formulário enviado!</span><p>Em breve entraremos em contato!</p></center></h2>";
+
+	// 	print "<html style=\"background: #fff;\"></html>";
+	// 	print "<meta HTTP-EQUIV='Refresh' CONTENT='2;URL=" . $site_url . "'>";
+	// }
+} 
+
+?>
---
+++ portfolio/site2/index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<title>Maxsuel Fernando - Portfólio/Site 2</title>
+	<link rel="shortcut icon" href="../../img/favicon.ico">
+	<meta charset="UTF-8">
+	<meta name="description" content="Maxsuel Fernando - Graduado em Sistemas de Informação, desenvolve páginas e sistemas Web.">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<meta property="og:url" content="https://maxsuelfernandob.000webhostapp.com/portfolio/site2"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:title" content="Maxsuel Fernando - Portfólio/Site 2">
+	<meta property="og:site_name" content="maxsuelfernandob">
+	<meta property="og:description" content="Maxsuel Fernando - Graduado em Sistemas de Informação, desenvolve páginas e sistemas Web."/>
+	<meta property="og:image" content="https://maxsuelfernandob.000webhostapp.com/portfolio/site2/img/intro-mobile.png"/>
+	<meta property="og:image:type" content="image/png">
+	<meta property="og:image:width" content="800"> 
+	<meta property="og:image:height" content="600">
+
+	<!-- <link rel="stylesheet" href="../../css/normalize.css">
+	<link rel="stylesheet" href="../../css/reset.css">
+	<link rel="stylesheet" href="../../css/grid.css">
+	<link rel="stylesheet" href="css/style-raw.css">
+	<link rel="stylesheet" href="css/responsivo.css"> -->
+
+	<link rel="stylesheet" href="css/style.css?<?php echo time();?>">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="nav-header">
+				<a href="index.html"><strong>logo</strong></a>	
+			</div>
+			<nav class="nav-content">
+				<ul>
+					<li>
+						<a href="#">Projects</a>
+					</li>
+					<li>
+						<a href="#">About us</a>
+					</li>
+					<li>
+						<a href="#">Contact</a>
+					</li>
+				</ul>		
+			</nav>
+		</div>
+	</header>
+	<div class="content">
+		<div class="titulo">
+			<h1>Lorem ipsum dolor</h1>
+		</div>
+		<div class="subtitulo">
+			<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis aliquid, iusto quas?</h2>
+		</div>
+		<div class="button">
+			<a href="#" class="view">View</a>
+		</div>
+	</div>
+</body>
+</html>
