533a1c43792aac83311b0d3f35d50d1c46e3aa08
Maxsuel Fernando <maxsuel.dev@gmail.com>
2023-03-04 22:06:22 -0300

    initial commit of project

--- portfolio/site1/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $(".rslides").responsiveSlides({
-		  speed: 800,            // Integer: Speed of the transition, in milliseconds
-		  timeout: 12000,          // Integer: Time between slide transitions, in milliseconds
-		  pager: true,           // Boolean: Show pager, true or false
-    });
-});
