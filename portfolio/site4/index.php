c45d2a85c3b5fa4f0cad3ce888045e974bf58d47
Maxsuel Fernando <maxsuel.dev@gmail.com>
2023-03-06 16:42:12 -0300

    add sites portfolio

---
+++ portfolio/site4/index.php
@@ -0,0 +1,240 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<title>Maxsuel Fernando - Portfólio/Site 4</title>
+	<link rel="shortcut icon" href="../../img/faviconM.ico">
+	<meta charset="UTF-8">
+	<meta name="description" content="Maxsuel Fernando - Graduado em Sistemas de Informação, desenvolve páginas e sistemas Web.">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta property="og:url" content="https://maxsuelfernandob.000webhostapp.com/portfolio/site4"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:title" content="Maxsuel Fernando - Portfólio/Site 4">
+	<meta property="og:site_name" content="maxsuelfernandob">
+	<meta property="og:description" content="Maxsuel Fernando - Graduado em Sistemas de Informação, desenvolve páginas e sistemas Web."/>
+	<meta property="og:image" content="https://maxsuelfernandob.000webhostapp.com/portfolio/site4/img/intro-mobile.png"/>
+	<meta property="og:image:type" content="image/png">
+	<meta property="og:image:width" content="800"> 
+	<meta property="og:image:height" content="600">
+	
+	<!-- <link rel="stylesheet" href="../../css/normalize.css">
+	<link rel="stylesheet" href="../../css/reset.css">
+	<link rel="stylesheet" href="../../css/grid.css">
+	<link rel="stylesheet" href="css/style-raw.css">
+	<link rel="stylesheet" href="css/responsivo.css"> -->
+
+	<link rel="stylesheet" href="css/style.css?<?php echo time();?>">
+</head>
+<body>
+	<header class="header">
+		<div class="container">
+			<a href="#" class="header_logo">LOGO</a>
+			<nav class="header_menu">
+				<ul>
+					<li>
+						<a href="#">Lorem ipsum</a>
+					</li>
+					<li>
+						<a href="#">dolor sit</a>
+					</li>
+					<li>
+						<a href="#">amet</a>
+					</li>
+					<li>
+						<a href="#">consectetur</a>
+					</li>
+				</ul>
+				<img src="img/menu.png" class="menu" alt="Menu">
+			</nav>
+		</div>
+		<nav class="nav-menu-responsivo">
+			<ul>
+				<li class="container">
+					<a href="#">Lorem ipsum</a>
+				</li>
+				<li class="container">
+					<a href="#">dolor sit</a>
+				</li>
+				<li class="container">
+					<a href="#">amet</a>
+				</li>
+				<li class="container">
+					<a href="#">consectetur</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+
+	<section class="intro">
+		<div class="container">
+			
+		</div>		
+	</section>
+
+	<section class="portfolio">
+		<div class="container">
+			<div class="portfolio_intro">
+				<span class="jobs-intro"><strong>Latest Jobs!</strong> We need your vote!</span>
+				<div class="portfolio_menu">
+					<ul>
+						<li>
+							<a href="#"><strong>Featured</strong></a>
+						</li>
+						<li>
+							<a href="#">Recent</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<!-- ITENS-PORTFOLIO -->
+
+			<div class="portfolio_box">
+				<div class="portfolio_itens">
+					<div class="grid-1-3 box-item">
+						<figure>
+							<a href="#">
+								<img src="img/jobs/item-1.jpg" alt="item-1" class="img-item">
+							</a>
+						</figure>
+						<div class="box-info">
+							<div class="box-info-line">
+								<a href="#" class="name-item"><strong>Lorem Ipsum</strong></a>
+							</div>
+							<div class="box-info-line">
+								By <a href="#" class="author-item"><strong>Maxsuel Fernando</strong></a> from Brazil
+							</div>
+							<div class="box-info-line">
+								January 31, 2017 in <a href="#" class="name-jobs"><strong>Jobs</strong></a>
+							</div>
+							<div class="box-info-line button-item">
+								<a href="#" class="btn-access">Access</a>
+							</div>
+						</div>
+					</div>
+					<div class="grid-1-3 box-item">
+						<figure>
+							<a href="#">
+								<img src="img/jobs/item-2.jpg" alt="item-2" class="img-item">
+							</a>
+						</figure>
+						<div class="box-info">
+							<div class="box-info-line">
+								<a href="#" class="name-item"><strong>Lorem Ipsum</strong></a>
+							</div>
+							<div class="box-info-line">
+								By <a href="#" class="author-item"><strong>Maxsuel Fernando</strong></a> from Brazil
+							</div>
+							<div class="box-info-line">
+								January 31, 2017 in <a href="#" class="name-jobs"><strong>Jobs</strong></a>
+							</div>
+							<div class="box-info-line button-item">
+								<a href="#" class="btn-access">Access</a>
+							</div>
+						</div>
+					</div>
+					<div class="grid-1-3 box-item">
+						<figure>
+							<a href="#">
+								<img src="img/jobs/item-3.jpg" alt="item-3" class="img-item">
+							</a>
+						</figure>
+						<div class="box-info">
+							<div class="box-info-line">
+								<a href="#" class="name-item"><strong>Lorem Ipsum</strong></a>
+							</div>
+							<div class="box-info-line">
+								By <a href="#" class="author-item"><strong>Maxsuel Fernando</strong></a> from Brazil
+							</div>
+							<div class="box-info-line">
+								January 31, 2017 in <a href="#" class="name-jobs"><strong>Jobs</strong></a>
+							</div>
+							<div class="box-info-line button-item">
+								<a href="#" class="btn-access">Access</a>
+							</div>
+						</div>
+					</div>
+
+					<div class="grid-1-3 box-item">
+						<figure>
+							<a href="#">
+								<img src="img/jobs/item-4.jpg" alt="item-4" class="img-item">
+							</a>
+						</figure>
+						<div class="box-info">
+							<div class="box-info-line">
+								<a href="#" class="name-item"><strong>Lorem Ipsum</strong></a>
+							</div>
+							<div class="box-info-line">
+								By <a href="#" class="author-item"><strong>Maxsuel Fernando</strong></a> from Brazil
+							</div>
+							<div class="box-info-line">
+								January 31, 2017 in <a href="#" class="name-jobs"><strong>Jobs</strong></a>
+							</div>
+							<div class="box-info-line button-item">
+								<a href="#" class="btn-access">Access</a>
+							</div>
+						</div>
+					</div>
+					<div class="grid-1-3 box-item">
+						<figure>
+							<a href="#">
+								<img src="img/jobs/item-5.jpg" alt="item-5" class="img-item">
+							</a>
+						</figure>
+						<div class="box-info">
+							<div class="box-info-line">
+								<a href="#" class="name-item"><strong>Lorem Ipsum</strong></a>
+							</div>
+							<div class="box-info-line">
+								By <a href="#" class="author-item"><strong>Maxsuel Fernando</strong></a> from Brazil
+							</div>
+							<div class="box-info-line">
+								January 31, 2017 in <a href="#" class="name-jobs"><strong>Jobs</strong></a>
+							</div>
+							<div class="box-info-line button-item">
+								<a href="#" class="btn-access">Access</a>
+							</div>
+						</div>
+					</div>
+					<div class="grid-1-3 box-item">
+						<figure>
+							<a href="#">
+								<img src="img/jobs/item-6.jpg" alt="item-6" class="img-item">
+							</a>
+						</figure>
+						<div class="box-info">
+							<div class="box-info-line">
+								<a href="#" class="name-item"><strong>Lorem Ipsum</strong></a>
+							</div>
+							<div class="box-info-line">
+								By <a href="#" class="author-item"><strong>Maxsuel Fernando</strong></a> from Brazil
+							</div>
+							<div class="box-info-line">
+								January 31, 2017 in <a href="#" class="name-jobs"><strong>Jobs</strong></a>
+							</div>
+							<div class="box-info-line button-item">
+								<a href="#" class="btn-access">Access</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- /ITENS-PORTFOLIO -->
+
+			<div class="load-more-itens">
+				<a href="#" class="btn-load-more-itens">Load More</a>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus vero rem sint consequatur neque distinctio.
+	</footer>
+
+	<script src="../../js/libs/jquery-3.1.1.min.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+</html>
